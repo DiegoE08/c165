5fcc93341e9a07fd483b1a1480d01665be78ca1f
DiegoE08 <86010342+DiegoE08@users.noreply.github.com>
2023-03-28 14:45:12 -0600

    weapon

--- enemyShoot.js
+++ enemyShoot.js
@@ -31,25 +31,55 @@ AFRAME.registerComponent("enemy-bullets", {
             scene.appendChild(enemyBullet);
 
             //Three.js Vector Variables
-            
+            var position1 = new THREE.Vector3();
+            var position2 = new THREE.Vecotr3();
+
+
+            //Direccion del disparo
+            var enemy = els[i].object3D;
+            var player = document.querySelector("#weapon").object3D;
+
 
             //Obtener la posición del enemigo y jugador usando el método Three.js 
-            
+            player.getWorldPosition(position1);
+            enemy.getWorldPosition(position2)
 
             //Establecer la velocidad y su dirección
-            
+            var direction = new THREE.Vector3();
+            direction.subVectors(position1, position2).normalize();
+            enemy.setAttribute("velocity", direction.multiplyScalar(10));
+
             //Establecer el atributo del cuerpo dinámico
-            
+            enemy.setAttribute("dynamic-body", {
+                shape: "sphere",
+                mass: "0",
+
+            });
+
 
             //Obtener atributo de texto
+            var element = document.querySelector("#countLife");
+            var playerLife = parseInt(element.getAttribute("text").value);
             
-
             //Evento de colisión con las balas enemigas
             enemyBullet.addEventListener("collide", function (e) {
                 if (e.detail.body.el.id === "weapon") {
 
                     //Agrega las condiciones aquí
-
+                    if (player.Life > 0) {
+                        playerLife = -1
+                        element.setAttribute("text",{
+                            value: playerLife
+                        });
+                    }
+                    if (playerLife <= 0) { 
+                        var txt = document.querySelector("#over");
+                        txt.setAttribute("visible", true);
+                        var tankEl = document.querySelectorAll(".enemy")
+                        for (var i = 0; i < tankEl.length; i++) {
+                            scene.removeChild(tankEl[i]);
+                        }
+                    }
 
 
                 }
